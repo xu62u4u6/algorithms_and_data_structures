de0ed1bd51ed99f32953f4b19574fafa44a2ca02
Ding-Yang Liu <199928ltyos@gmail.com>
2022-11-04 22:58:11 +0800

    add merge sort

--- algorithm/sort/merge_sort/README.md
+++ algorithm/sort/merge_sort/README.md
@@ -1,15 +1,34 @@
 # 合併排序 Merge Sort
-Divide and Conquer的例子
+### 原理
+---
+將序列每次分為兩半直到剩下一個元素，後兩兩合併。
+```python
+def mergesort(arr, start, end):
+        if end > start:    
+            mid = ceil((start+end)/2)
+            mergesort(arr, start, mid)
+            mergesort(arr, mid+1, end)
+            merge(arr, start, mid, end)
 
-```psuedocode
-psuedo code:
-MERGE():
-    2 array 比較頭部裝入
-    L = p q-1
-    R = Q R
-
-    for i in (L, R):
-        較小
-    2指針 比較 較小換至頭部
-
+def merge(arr, start, mid, end):
+        L = arr[start: mid+1]
+        R = arr[mid+1: end]
+        L_ind, R_ind = 0
+        L R 依序將較小的放入arr[i]
 ```
+### 優缺
+---
+* 優點: 較快速度
+* 缺點: 對已經排序的序列並不會加速、較小序列合併浪費時間
+
+### 時間複雜度
+---
+Merge sort 的時間複雜度為 $O(nlg{n})$，
+
+### 空間複雜度
+---
+需要$O(n)$的額外空間 - $L, R$
+
+### 改進
+---
+可以針對較小序列使用將MERGE()用Insertion sort改寫
--- algorithm/sort/merge_sort/mergesort.cc
+++ algorithm/sort/merge_sort/mergesort.cc
@@ -1,32 +1,87 @@
 #include <iostream>
-#include <math.h>; 
-
+#include <math.h>
 using namespace std;
-void merge(int p, int q, int r);
-void merge_sort(int p, int r);
+
+void print_arr(int arr[], int start, int end){
+    for(int i=start; i<=end; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+};
+
+void merge(int arr[], int start, int mid, int end){
+
+    int l_n = mid-start+1; // 6-0+1 = 7
+    int r_n = end-mid;   // 12-6 = 7
+    int l_arr[l_n], r_arr[r_n];
+    
+    // 裝入左右數值
+    for(int i=0; i<l_n; i++){
+        l_arr[i] = arr[start+i];
+    }
+
+    for(int i=0; i<r_n; i++){
+        r_arr[i] = arr[mid+1+i];
+    }
+
+    int l_ind=0, r_ind=0, i=start;
+
+    // 左右比較並把較小值加入arr
+    while(l_ind<l_n && r_ind<r_n){
+        if (l_arr[l_ind]<=r_arr[r_ind]){
+            arr[i] = l_arr[l_ind];
+            l_ind++;
+        }
+        else{
+            arr[i] = r_arr[r_ind];
+            r_ind++;
+        }
+        i++;
+    }   
+    // 如果單邊太小先跑完情況，就把剩下的加完
+    while(l_ind<l_n){
+        arr[i] = l_arr[l_ind];
+        l_ind++;
+        i++;
+
+    }
+    while(l_ind<l_n){
+        arr[i] = r_arr[r_ind];
+        r_ind++;
+        i++;
+    }
+
+    print_arr(l_arr, 0, l_n-1);
+    print_arr(r_arr, 0, r_n-1);
+    print_arr(arr, start, end);
+    cout << endl;
+};
+
+//baseline: 只有一個元素時不跑
+void merge_sort(int arr[], int start, int end){
+    int mid = ceil((start+end)/2);
+    // if n>1
+    if(end > start){
+        merge_sort(arr, start, mid);
+        merge_sort(arr, mid+1, end);
+        merge(arr, start, mid, end);
+    }
+};
+
 
 int main(){
+    int test_arr[] = {4, 3, 2, 6, 8, 1, 9, 2, 7};
+    int n = (int)(sizeof(test_arr)/sizeof(test_arr[0]));
+    cout << "unsort:";
+    print_arr(test_arr, 0, n-1);
+    cout << endl;
+    merge_sort(test_arr, 0, n-1);
 
-
-
+    cout << "sorted:";
+    print_arr(test_arr, 0, n-1);
+    cout << endl;
+    //merge(test_arr)
+    //cout << ceil(5/2) << endl;
+    //cout << ceil(6/2) << endl;
     return 0;
 }
-
-void merge(int p, int q, int r){
-
-    int l_arr_n = q-p+1 // 6-0+1 = 7
-    int r_arr_n = r-q   // 12-6 = 7
-    
-    
-    // 將值裝入L中
-    // 將值裝入R中
-
-}
-
-void merge_sort(int p, int r){
-    int q = ceil((p+r-1)/2);
-    merge_sort(p, q-1);
-    merge_sort(q, r);
-    merge(p, q, r);
-}
-
